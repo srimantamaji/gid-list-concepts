f1c711c448ffaceebf27bdef1d3daebd2feef82b
Shreya Bharadwaj <shreyab@nrifintech.com>
2017-08-03 11:27:30 +0530

    Fetched latest changes of components

---
+++ components/gid-api/assets/js/gidRestApi.js
@@ -0,0 +1,20 @@
+var apis = {
+		"lobs": "/lobs",
+		"concepts": "/lobs/{lob}/concepts",
+		"applications": "/concepts/{concept}/applications",
+		"databases": "/concepts/{concept}/applications/{application}/databases",
+		"schemas": "/concepts/{concept}/applications/{application}/databases/{database}/schemas",
+		"tables": "/concepts/{concept}/applications/{application}/databases/{database}/schemas/{schema}/tables",
+		"columns": "/concepts/{concept}/applications/{application}/databases/{database}/schemas/{schema}/tables/{table}/columns",
+		"coverage": "/concepts/{concept}/coverage",
+		"table-columns": "/tables/{table}/columns",
+		"table-data": "/tables/{table}/rows",
+		"recommended-concepts": "/columns/{column}/recommendedconcepts",
+		"verify-concept": "/columns/{column}/recommendedconcepts/{concept}/verify",
+		"samples": "/columns/{column}/samples",
+		"synonyms": "/columns/{column}/synonyms",
+		"patterns": "/columns/{column}/patterns",
+		"list-concepts": "/concepts",
+		"concept-details" : "/concepts/{concept}",
+		"curate-concept" : "/concepts/{concept}/curate"
+}
